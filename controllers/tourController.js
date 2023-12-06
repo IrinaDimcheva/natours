c9a01bcfc374a4355691c3e4becf46584ece3875
idimcheva <dimchevairina@gmail.com>
2023-12-06 02:36:00 +0200

    Model the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -54,7 +54,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
