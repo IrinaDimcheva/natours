af1dd852c17e74823c97dbd0a08cbae7d83398e0
iamirinad <irka_dimcheva@abv.bg>
2023-12-13 18:26:34 +0200

    Handle errors outside express - unhandled rejections

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ mongoose.connect(DB).then(() => console.log('Connection successful!'));
 //   .then(() => console.log('Connected to Database'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
