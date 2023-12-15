3defa230c2e3aa3628f47ddccf19497df3e1b572
iamirinad <irka_dimcheva@abv.bg>
2023-12-15 02:40:10 +0200

    Protect tour routes

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -53,6 +54,20 @@ userSchema.methods.correctPassword = async function (
 ) {
   // returns boolean
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+
+    return JWTTimestamp < changedTimestamp; // 300 < 200
+  }
+
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
