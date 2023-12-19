d9eb377f1ada3a0cb1dca390abfc6a8b5343117b
Irina Dimcheva <dimchevairina@gmail.com>
2023-12-19 23:41:56 +0200

    Build handler factory delete function

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,16 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that id', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
