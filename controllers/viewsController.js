97e6ebcf5b65f8b95b6257773dca9d0f3f6541f9
Irina Dimcheva <dimchevairina@gmail.com>
2023-12-29 18:32:51 +0200

    Build tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
+  });
+
+  // 2) Build template
+  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
