e06e9c64010844892ec454fd50036989a789e67d
Irina Dimcheva <dimchevairina@gmail.com>
2023-12-29 23:17:31 +0200

    Include map with leaflet

--- app.js
+++ app.js
@@ -26,6 +26,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
 app.use(helmet());
 
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+//set security http headers
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  }),
+);
+
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
---
+++ public/js/mapbox.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+var map = L.map('map', { zoomControl: false }); //to disable + - zoom
+// var map = L.map('map', { zoomControl: false }).setView([31.111745, -118.113491], );
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  crossOrigin: '',
+}).addTo(map);
+
+const points = [];
+locations.forEach((loc) => {
+  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  L.marker([loc.coordinates[1], loc.coordinates[0]])
+    .addTo(map)
+    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+      autoClose: false,
+    })
+    .openPopup();
+});
+
+const bounds = L.latLngBounds(points).pad(0.5);
+map.fitBounds(bounds);
+
+map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
