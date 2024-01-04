5c6fd39b13a0c81b3b7770b9376b6ea37587bb7c
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-05 01:09:53 +0200

    Save image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,8 +42,8 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
+  // console.log(req.file);
+  // console.log(req.body);
 
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -57,6 +57,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filter out unwanted field names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- models/userModel.js
+++ models/userModel.js
@@ -15,9 +15,13 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: String,
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
   role: {
     type: String,
+    // enum: ['user', 'guide', 'lead-guide', 'admin'],
     default: 'user',
   },
   password: {
