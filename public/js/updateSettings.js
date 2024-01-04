6ef68e5a5f8ee3b5d9e927ba1064a89e66cc4029
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-04 02:05:01 +0200

    Update user password with the api

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,27 +1,24 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
-    console.log(res, res.data);
-
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
-      // window.setTimeout(() => {
-      //   location.assign('/me');
-      // }, 1500);
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
-    console.log(err);
     showAlert('error', err.response.data.message);
   }
 };
