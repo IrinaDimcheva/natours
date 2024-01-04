4afa7fcf4cd95aafe956580b47899f86715103ae
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-05 00:45:22 +0200

    Configure multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,27 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-const upload = multer({ dest: 'public/img/users' });
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    return cb(null, true);
+  }
+  cb(new AppError('Not an image! Please upload only images.', 400), false);
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
 exports.uploadUserPhoto = upload.single('photo');
 
