9e81d25e436ad175e8d47f300c7c98411c148f9f
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-10 15:08:32 +0200

    Fix stripe

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
-// app.enable('trust proxy');
-
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,8 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+
     // React setup
     // secure: true,
     // sameSite: 'none',
@@ -27,7 +28,7 @@ const createSendToken = (user, statusCode, req, res) => {
   //   cookieOptions.secure = true;
   // }
 
-  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
 
